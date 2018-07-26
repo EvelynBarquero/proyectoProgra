6f82514736d90bb259186eff6ac7b80083ce119d
kovthe27 <39032531+kovthe27@users.noreply.github.com>
2018-07-25 18:18:58 -0600

    Controlador_Register

--- interfazWeb/src/js/registrar/controlador_registrar.js
+++ interfazWeb/src/js/registrar/controlador_registrar.js
@@ -1,9 +1,50 @@
 "use strict";
 
-inputPrimerNombre=document.querySelector("#txtPrimerNombre");
-inputSegundoNombre=document.querySelector("#txtSegundoNombre");
-inputCedula=document.querySelector('#txtCedula');
-inputSexo=document.querySelector("#txtSexo");
-//fecha
-inputTelefono=document.querySelector("#txtTelefono");
-inputPassword=document.querySelector("#txtPassword");
+const inputPrimerNombre=document.querySelector("#txtPrimerNombre");
+const inputSegundoNombre=document.querySelector("#txtSegundoNombre");
+const inputCedula=document.querySelector('#txtCedula');
+const inputSexo=document.querySelector("#txtSexo");
+const inputFecha=document.querySelector("#txtFecha");
+const inputTelefono=document.querySelector("#txtTelefono");
+const inputPassword=document.querySelector("#txtPassword");
+const inputConfirmarPassword=document.querySelector("#txtConfirmarPassword");
+
+const botonCrearCuenta=document.querySelector("#btnCrearCuenta");
+
+botonCrearCuenta.addEventListener("click", crearCuenta);
+
+let primerNombre=inputPrimerNombre.value;
+let segundoNombre=inputSegundoNombre;
+let cedula=inputCedula;
+let sexo=inputSexo;
+let fecha= new Date (inputFecha);
+let telefono=inputTelefono;
+let password=inputPassword;
+let confirmacion=inputConfirmarPassword;
+let edad=0;
+
+function crearCuenta(){
+
+    // edad=calcularEdad(fecha); //Calcula la edad apartir de la fecha
+
+    let bUsuario=0;
+    //bUsuario=validarUsuario()
+    validarInputs();
+    let test=inputPrimerNombre.value;
+
+};
+
+function validarInputs(){
+    let bError=false;
+    let regexSoloLetras = /^[a-zA-Z]+$/;
+    let regexSoloDigitos = /^[0-9]{1,3}$/;
+    primerNombre;
+
+    if(primerNombre == "" || regexSoloLetras.test(primerNombre)==false){
+        bError = true;
+    }else{
+        console.log("error");
+    }
+    console.log(bError);
+};
+
