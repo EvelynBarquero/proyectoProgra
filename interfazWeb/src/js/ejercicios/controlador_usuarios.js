199b30b5a9df3b2fb0e6132b67fa4b0bf293129b
ebarquero <ebarquero@altus.co.cr>
2018-07-31 17:22:43 -0600

    nombreOpcional_ejercicios

--- interfazWeb/src/js/ejercicios/controlador_usuarios.js
+++ interfazWeb/src/js/ejercicios/controlador_usuarios.js
@@ -2,36 +2,36 @@
 
 // const inputCedula = document.querySelector('#txtCedula');
 const imagenFoto = document.querySelector('#imgFoto');
-const inputNombre =  document.querySelector('#txtNombre');
-const inputRepeticiones = document.querySelector('#txtRepeticiones');
+const inputNombre = document.querySelector('#txtNombre');
+const inputNombreOpcional = document.querySelector('#txtNombreOpcional');
 const inputCategoria = document.querySelector('#sltCategoria');
 
 
 const botonRegistrar = document.querySelector('#btnRegistrar');
 
-const mostrarTablaUsuarios = () =>{
+const mostrarTablaUsuarios = () => {
     let mListaUsuarios = obtenerListaUsuarios();
     let tbody = document.querySelector('#tblUsuarios tbody');
     tbody.innerHTML = '';
 
-    for(let i = 0; i < mListaUsuarios.length; i++){
+    for (let i = 0; i < mListaUsuarios.length; i++) {
         let fila = tbody.insertRow();
 
         let celdaFoto = fila.insertCell();
         let celdaNombre = fila.insertCell();
         let celdaCategoria = fila.insertCell();
-        let celdaRepeticiones = fila.insertCell();
+        let celdaNombreOpcional = fila.insertCell();
 
         celdaNombre.innerHTML = mListaUsuarios[i][1];
         celdaCategoria.innerHTML = mListaUsuarios[i][2];
-        celdaRepeticiones.innerHTML = mListaUsuarios[i][3];
+        celdaNombreOpcional.innerHTML = mListaUsuarios[i][3];
 
         let imagen = document.createElement('img');
         imagen.classList.add('imagen_tabla');
 
         let imagenUrl = mListaUsuarios[i][0];
-        
-        if(imagenUrl == ''){
+
+        if (imagenUrl == '') {
             imagenUrl = '../img/ejercicios/user_placeholder.png';
         };
 
@@ -42,72 +42,68 @@ const mostrarTablaUsuarios = () =>{
 };
 
 
-const limpiarFormulario = () =>{
+const limpiarFormulario = () => {
     inputNombre.value = '';
     inputCategoria.value = '';
-    inputRepeticiones.value = '';
+    inputNombreOpcional.value = '';
     imagenFoto.src = '../img/ejercicios/user_placeholder.png';
 };
+
+
 let sNombre = inputNombre.value;
 let sCategoria = inputCategoria.value;
 
-const obtenerDatosRegistro = () =>{
+const obtenerDatosRegistro = () => {
     let aNuevoUsuario = [];
 
     // let sCedula = inputCedula.value;
     // let sNombre = inputNombre.value;
     // let sCategoria = inputCategoria.value;
-    let sRepeticiones = inputRepeticiones.value;
+    let sNombreOpcional = inputNombreOpcional.value;
     let sImagenUrl = imagenFoto.src;
 
     // Validación de error
     let bError = false;
     bError = validar();
-    if(bError==true){
+    if (bError == true) {
         swal({
-            type : 'warning',
+            type: 'warning',
             title: 'Campos vacios', //Ver documentación https://cdn.jsdelivr.net/npm/sweetalert2
             text: 'No se pudo realizar el registro',
         })
         // console.log('No se pudo registrar el usuario');
-    }
-    else {
+    } else {
         swal({
-            type : 'success',
+            type: 'success',
             title: 'Perfecto', //Ver documentación https://cdn.jsdelivr.net/npm/sweetalert2
             text: 'Registro exitoso',
         })
     }
 
-    aNuevoUsuario.push(sImagenUrl, sNombre, sCategoria, sRepeticiones);
+    aNuevoUsuario.push(sImagenUrl, sNombre, sCategoria, sNombreOpcional);
     registrarUsuario(aNuevoUsuario);
     mostrarTablaUsuarios();
     limpiarFormulario();
 
 };
 
+// botonRegistrar.addEventListener('click', obtenerDatosRegistro);
 
-function validar(){
+function validar() {
     let bError = false;
     let regexSoloLetras = /^[a-zA-Z]+$/;
     // let regexSoloNumeros = /^[0-9]{1,3}$/; //el uno y 3 son el mínimo y máximo de caracteres
-    
+
     //Validación del campo Nombre1
-    if(sNombre == '' || regexSoloLetras.test(sNombre)==false ){
+    if (sNombre == '' || regexSoloLetras.test(sNombre) == false) {
         bError = true;
         inputNombre.classList.add('input-error');
-    }
-    else{ //dejar el else vacío porque en caso de que el apellido esté incorrecto, pone todo como error
+    } else { //dejar el else vacío porque en caso de que el apellido esté incorrecto, pone todo como error
         // inputNombre.classList.remove('input-error');
     }
-   
+
     return bError;
 }
 
 mostrarTablaUsuarios();
 botonRegistrar.addEventListener('click', obtenerDatosRegistro);
-
-
-
-
-
