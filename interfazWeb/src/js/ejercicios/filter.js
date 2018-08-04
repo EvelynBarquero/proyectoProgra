e67e656a8ede8ae5270e88e763d858723de5a1fc
ebarquero <ebarquero@altus.co.cr>
2018-08-04 11:07:32 -0600

    filtrarEjecicio

--- interfazWeb/src/js/ejercicios/filter.js
+++ interfazWeb/src/js/ejercicios/filter.js
@@ -27,28 +27,28 @@ $TableFilter = function(id, value){
 
 
 
-document.querySelector("#buscar").onkeyup = function(){
-    $TableFilter("#tblInstructores", this.value);
-}
+// document.querySelector("#buscar").onkeyup = function(){
+//     $TableFilter("#tblInstructores", this.value);
+// }
 
-$TableFilter = function(id, value){
-    var rows = document.querySelectorAll(id + ' tbody tr');
+// $TableFilter = function(id, value){
+//     var rows = document.querySelectorAll(id + ' tbody tr');
     
-    for(var i = 0; i < rows.length; i++){
-        var showRow = false;
+//     for(var i = 0; i < rows.length; i++){
+//         var showRow = false;
         
-        var row = rows[i];
-        row.style.display = 'none';
+//         var row = rows[i];
+//         row.style.display = 'none';
         
-        for(var x = 0; x < row.childElementCount; x++){
-            if(row.children[x].textContent.toLowerCase().indexOf(value.toLowerCase().trim()) > -1){
-                showRow = true;
-                break;
-            }
-        }
+//         for(var x = 0; x < row.childElementCount; x++){
+//             if(row.children[x].textContent.toLowerCase().indexOf(value.toLowerCase().trim()) > -1){
+//                 showRow = true;
+//                 break;
+//             }
+//         }
         
-        if(showRow){
-            row.style.display = null;
-        }
-    }
-}
+//         if(showRow){
+//             row.style.display = null;
+//         }
+//     }
+// }
