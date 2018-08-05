d3624a11306c6fc2cdbb4b58be5924be461d289b
ebarquero <ebarquero@altus.co.cr>
2018-08-04 20:48:07 -0600

    perimetros

---
+++ interfazWeb/src/js/perimetros/controladorPerimetros.js
@@ -0,0 +1,223 @@
+'use strict';
+
+// const inputImagen = document.querySelector('#btnSeleccionarImagen');
+const inputFecha = document.querySelector('#txtFecha');
+const inputBicepDerecho = document.querySelector('#txtBicepDerecho');
+const inputBicepIzquierdo  = document.querySelector('#txtBicepIzquierdo');
+const inputBicepContDerecho= document.querySelector('#txtBicepContDerecho');
+const inputBicepContIzquierdo = document.querySelector ('#txtBicepContIzquierdo');
+const inputPantorrillaDerecha = document.querySelector ('#txtPantorrillaDerecha');
+const inputPantorrillaIzquierda = document.querySelector ('#txtPantorrillaIzquierda');
+const inputMusloDerecho = document.querySelector ('#txtMusloDerecho');
+const inputMusloIzquierdo = document.querySelector ('#txtMusloIzquierdo');
+const inputCintura = document.querySelector ('#txtCintura');
+const inputAbdomen = document.querySelector ('#txtAbdomen');
+const inputCadera = document.querySelector ('#txtCadera');
+const inputPecho = document.querySelector ('#txtPecho');
+const inputEspalda = document.querySelector ('#txtEspalda');
+
+const botonGuardar = document.querySelector('#btnGuardar');
+const botonActualizar = document.querySelector('#btnActualizar');
+
+botonActualizar.hidden = true;
+
+// function obtenerInstructorPorCedula (){
+//     botonRegistrar.hidden = true;
+//     botonActualizar.hidden = false;
+
+//     let sCedula = this.dataset.cedula;
+//     let instructor = buscarInstructorPorCedula(sCedula);
+
+//     inputNombre.value = instructor[0];
+//     inputSegundoNombre.value = instructor[1];
+//     inputPrimerApellido.value = instructor[2];
+//     inputSegundoApellido.value = instructor[3];
+//     inputCedula.value = instructor[4];
+//     inputCedula.disabled = true;
+
+//     inputFechaNacimiento.value = instructor[5];
+//     inputEdad.value = instructor[6];
+//     inputSexo.option = instructor[7];
+//     inputTelefono.value = instructor[8];
+//     inputUsuario.value = instructor[9];
+//     inputContrasenna.value = instructor[10];
+
+//     if(instructor[11] == ''){
+//         imagenFoto.src = 'img/user_placeholder.png';
+//     }else{
+//         imagenFoto.src = instructor[11];
+//     }
+
+// };
+
+const mostrarTablaMediciones = () => {
+    let mListaMediciones = obtenerListaMediciones();
+    let tbody = document.querySelector('#tblPerimetros tbody');
+    tbody.innerHTML = '';
+
+    for (let i = 0; i < mListaMediciones.length; i++) {
+        let fila = tbody.insertRow();
+
+        let celdaFecha  = fila.insertCell();
+        let celdaBicepDerecho  = fila.insertCell();
+        let celdaBicepIzquierdo  = fila.insertCell();
+        let celdaBicepContDerecho = fila.insertCell();
+        let celdaBicepContIzquierdo  = fila.insertCell();
+        let celdaPantorrillaDerecha = fila.insertCell();
+        let celdaPantorrillaIzquierda = fila.insertCell();
+        let celdaMusloDerecho = fila.insertCell();
+        let celdaMusloIzquierdo = fila.insertCell();
+        let celdaCintura = fila.insertCell();
+        let celdaAbdomen = fila.insertCell();
+        let celdaCadera = fila.insertCell();
+        let celdaPecho = fila.insertCell();
+        let celdaEspalda= fila.insertCell();
+
+       
+
+        // celdaFoto.innerHTML = mListaInstructores[i][0];
+        celdaFecha.innerHTML = mListaMediciones[i][0];
+        celdaBicepDerecho.innerHTML = mListaMediciones[i][1];
+        celdaBicepIzquierdo.innerHTML = mListaMediciones[i][2];
+        celdaBicepContDerecho.innerHTML = mListaMediciones[i][3];
+        celdaBicepContIzquierdo.innerHTML = mListaMediciones[i][4];
+        celdaPantorrillaDerecha.innerHTML = mListaMediciones[i][5];
+        celdaPantorrillaIzquierda.innerHTML = mListaMediciones[i][6];
+        celdaMusloDerecho .innerHTML = mListaMediciones[i][7];
+        celdaMusloIzquierdo.innerHTML = mListaMediciones[i][8];
+        celdaCintura.innerHTML = mListaMediciones[i][9];
+        celdaAbdomen.innerHTML = mListaMediciones[i][10];
+        celdaCadera.innerHTML = mListaMediciones[i][11];
+        celdaPecho.innerHTML = mListaMediciones[i][12];
+        celdaEspalda.innerHTML = mListaMediciones[i][13];
+
+       
+        // let imagen = document.createElement('img');
+        // imagen.classList.add('imagen_tabla');
+
+        // let imagenUrl = mListaInstructores[i][11];
+
+        // if (imagenUrl == '') {
+        //     imagenUrl = '../img/ejercicios/user_placeholder.png';
+        // };
+
+        // imagen.src = imagenUrl;
+
+        // celdaFoto.appendChild(imagen);
+
+        // let botonIconoEditar = document.createElement('a');
+        // botonIconoEditar.classList.add('fa');
+        // botonIconoEditar.classList.add('fa-edit');
+        // botonIconoEditar.dataset.cedula = mListaMediciones[i][0];
+
+        // botonIconoEditar.addEventListener('click' , obtenerInstructorPorCedula);
+        // botonIconoEditar .addEventListener('click' , show);
+
+        // celdaConfiguracion.appendChild(botonIconoEditar);
+    };
+};
+
+
+
+const limpiarFormulario = () => {
+    inputFecha.value = 0;
+    inputBicepDerecho.value = 0;
+    inputBicepIzquierdo.value = 0;
+    inputBicepContDerecho.value = 0;
+    inputBicepContIzquierdo.value = 0;
+    inputPantorrillaDerecha.value = 0;
+    inputPantorrillaIzquierda.value = 0;
+    inputMusloDerecho.value = 0;
+    inputMusloIzquierdo.value = 0;
+    inputCintura.value = 0;
+    inputAbdomen.value = 0;
+    inputCadera.value = 0;
+    inputPecho.value = 0;
+    inputEspalda.value = 0;
+};
+
+
+
+const obtenerDatosRegistro = () => {
+    let aNuevaMedida = [];
+
+    let dFecha = inputFecha.value;
+    let nBicepDerecho = inputBicepDerecho.value;
+    let nBicepIzquierdo = inputBicepIzquierdo.value;
+    let nBicepContDerecho = inputBicepContDerecho.value;
+    let nBicepContIzquierdo = inputBicepContIzquierdo.value;
+    let nPantorrillaDerecha = inputPantorrillaDerecha.value;
+    let nPantorrillaIzquierda = inputPantorrillaIzquierda.value;
+    let nMusloDerecho = inputMusloDerecho.value;
+    let nMusloIzquierdo = inputMusloIzquierdo.value;
+    let nCintura = inputCintura.value;
+    let nAbdomen = inputAbdomen.value
+    let nCadera = inputCadera.value
+    let nPecho = inputPecho.value
+    let nEspalda = inputEspalda.value
+
+    aNuevaMedida.push(dFecha , nBicepDerecho,nBicepIzquierdo, nBicepContDerecho, nBicepContIzquierdo, nPantorrillaDerecha, nPantorrillaIzquierda, nMusloDerecho, nMusloIzquierdo, nCintura, nAbdomen, nCadera, nPecho, nEspalda);
+    registrarMedida(aNuevaMedida);
+    mostrarTablaMediciones();
+    limpiarFormulario();
+    
+};
+
+
+// const obtenerDatosActualizar = () =>{
+//     let aUsuarioModificado = [];
+
+//     let sImagenUrl = imagenFoto.src;
+//     let sNombre = inputNombre.value;
+//     let sSegundoNombre = inputSegundoNombre.value;
+//     let sPrimerApellido = inputPrimerApellido.value;
+//     let sSegundoApellido = inputSegundoApellido.value;
+//     let sCedula = inputCedula.value;
+//     let dFechaNacimiento = inputFechaNacimiento.value;
+//     let nEdad = Number(inputEdad.value);
+//     let sSexo = inputSexo.value;
+//     let nTelefono = inputTelefono.value
+//     let sUsuario = inputUsuario.value
+//     let sContrasenna = inputContrasenna.value
+
+
+//     aUsuarioModificado.push(sNombre,sSegundoNombre, sPrimerApellido, sSegundoApellido, sCedula, dFechaNacimiento, nEdad, sSexo, nTelefono, sUsuario, sContrasenna, sImagenUrl );
+    
+//     modificarUsuario(aUsuarioModificado);
+//     mostrarTablaInstructores();
+//     limpiarFormulario();
+//     botonActualizar.hidden = true;
+//     botonRegistrar.hidden = false;
+//     inputCedula.disabled = false;
+
+// };
+
+
+mostrarTablaMediciones();
+botonGuardar.addEventListener('click', obtenerDatosRegistro);
+// botonActualizar.addEventListener('click', obtenerDatosActualizar);
+
+botonGuardar .addEventListener('click' , hide);
+// botonActualizar .addEventListener('click' , hide);
+
+
+// ------------------
+
+function show() {
+    var x = document.getElementById("tableEdit");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function hide() {
+    var x = document.getElementById("tableEdit");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
---
+++ interfazWeb/src/js/perimetros/servicioPerimetros.js
@@ -0,0 +1,47 @@
+'use strict';
+
+
+const obtenerListaMediciones = () =>{
+    let mListaMediciones = JSON.parse(localStorage.getItem('listaMedicionesLS'));
+    if(mListaMediciones == null){
+        mListaMediciones = [
+            ['2017-06-22','23','34', '54','35','35','6', '22', '46','46','12','67','35', '23']
+        ];
+    }
+
+    return mListaMediciones;
+};
+
+const registrarMedida = (paNuevaMedida) =>{
+    let mListaMediciones = obtenerListaMediciones();
+
+    mListaMediciones.push(paNuevaMedida);
+
+    localStorage.setItem('listaMedicionesLS', JSON.stringify(mListaMediciones));
+}; 
+
+
+// const buscarInstructorPorCedula = (pCedula) =>{
+//     let mListaInstructores = obtenerListaInstructores();
+//     let usuarioEncontrado = [];
+//     for(let i = 0; i < mListaInstructores.length; i++ ){
+//         if(mListaInstructores[i][0] == pCedula){
+//             usuarioEncontrado = mListaInstructores[i];
+//         }
+//     };
+//     return usuarioEncontrado;
+// };
+
+
+
+// pUsuarioModificado -> arreglo
+// const modificarUsuario = (pUsuarioModificado) =>{
+//     let mListaInstructores = obtenerListaInstructores();
+//     for(let i = 0 ; i < mListaInstructores.length; i++ ){
+//         if(mListaInstructores[i][0] == pUsuarioModificado[0]){
+//             mListaInstructores[i] = pUsuarioModificado;
+//         }
+//     }
+//     localStorage.setItem('listaInstructoresLS', JSON.stringify(mListaInstructores));
+
+// };
